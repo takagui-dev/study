5de07ff16d5e743048493af2e642309d41426d24
Guilherme Takahashi <gui.yukio175@gmail.com>
2021-11-03 00:59:48 -0300

    feat: create action add product to cart

--- redux-app/src/components/Catalog.tsx
+++ redux-app/src/components/Catalog.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import api from '../services/api';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
+import api from '../services/api';
 import { IProduct } from '../store/modules/cart/types';
+import { addProductToCart } from '../store/modules/cart/actions';
 
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -11,6 +14,10 @@ const Catalog: React.FC = () => {
       setCatalog(response.data);
     })
   }, []);
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product));
+  }, [dispatch]);
 
   return (
     <main>
@@ -23,7 +30,12 @@ const Catalog: React.FC = () => {
           <span>{product.price}</span>
           {'   '}
 
-          <button type="button">Comprar</button>
+          <button
+            type="button"
+            onClick={() => handleAddProductToCart(product)}
+          >
+            Comprar
+          </button>
         </article>
       ))}
     </main>
