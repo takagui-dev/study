e302fdaaefd251699b8f94aba3a9239e7ab9df00
Guilherme Takahashi <gui.yukio175@gmail.com>
2021-11-04 13:09:30 -0300

    feat: add quantity

--- redux-app/src/store/modules/cart/reducer.ts
+++ redux-app/src/store/modules/cart/reducer.ts
@@ -12,11 +12,18 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
     switch(action.type) {
       case 'ADD_PRODUCT_TO_CART': {
         const { product } = action.payload;
+        const productInCartIndex = draft.items.findIndex(item =>
+          item.product.id === product.id,
+        );
 
-        draft.items.push({
-          product,
-          quantity: 1,
-        });
+        if (productInCartIndex >= 0) {
+          draft.items[productInCartIndex].quantity++;
+        } else {
+          draft.items.push({
+            product,
+            quantity: 1,
+          });
+        }
 
         break;
       }
